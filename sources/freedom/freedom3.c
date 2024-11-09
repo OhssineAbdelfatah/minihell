2b94a0e54493c9663287a9735d169760be48ff41
iliass-laa <iliasslaasri1999@gmail.com>
2024-11-09 15:54:41 +0100

    norming ..

--- sources/freedom/freedom3.c
+++ sources/freedom/freedom3.c
@@ -1,21 +1,31 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   freedom3.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilaasri <ilaasri@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/09 15:54:06 by ilaasri           #+#    #+#             */
+/*   Updated: 2024/11/09 15:54:08 by ilaasri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void free_dawgs(int how_many,...)
+void	free_dawgs(int how_many, ...)
 {
-    int i;
-    char **dawg;
+	int		i;
+	char	**dawg;
+	va_list	lst;
 
-    i = 0;
-    va_list lst;
-    dawg = NULL;
-    va_start(lst, how_many);
-    while (i < how_many)
-    {
-        dawg = va_arg(lst, char **);
-        free_mynigga(dawg);
-        i++;
-    }
-    va_end(lst);
+	i = 0;
+	dawg = NULL;
+	va_start(lst, how_many);
+	while (i < how_many)
+	{
+		dawg = va_arg(lst, char **);
+		free_mynigga(dawg);
+		i++;
+	}
+	va_end(lst);
 }
