42f4470d96c8c7c7edd7d97f9d58e5ba7403166e
Abdelfatah Ohssine <aohssine@c2r11p2.1337.ma>
2024-11-09 11:50:15 +0100

    lila

--- sources/expander/expand_heredoc.c
+++ sources/expander/expand_heredoc.c
@@ -14,6 +14,7 @@ int herdoc_newfd( int fd, t_env* myenv)
             break;
         tmp = line;
         line = splitWordVar(line, myenv, 0);
+        line = whithout_quotes(line);
         free(tmp);
         if(line)
             ft_putstr_fd(line, fd_pipe[1]);
